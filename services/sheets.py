496eb9f18c31c09180bb29d2a595999d65d585e6
Pritish Barik <pritish.sec@gmail.com>
2026-04-15 18:26:17 +0530

    code fix for env vars

--- services/sheets.py
+++ services/sheets.py
@@ -1,4 +1,5 @@
 import gspread
+import os
 import json
 import streamlit as st
 from google.oauth2.service_account import Credentials
@@ -8,30 +9,30 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds_dict = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
+creds_dict = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
 
 creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 client = gspread.authorize(creds)
 
 
 def get_stock():
-    sheet = client.open_by_key(st.secrets["STOCK_SHEET_ID"]).worksheet("STOCK")
+    sheet = client.open_by_key(os.environ["STOCK_SHEET_ID"]).worksheet("STOCK")
     return sheet.get_all_records()
 
 
 def get_price_map():
-    sheet = client.open_by_key(st.secrets["STOCK_SHEET_ID"]).worksheet(st.secrets["PRICE_SHEET_NAME"])
+    sheet = client.open_by_key(os.environ["STOCK_SHEET_ID"]).worksheet(os.environ["PRICE_SHEET_NAME"])
     data = sheet.get_all_records()
     return {str(i["LN_CODE"]).strip(): int(i["MRP"]) for i in data}
 
 
 def write_order(row):
-    sheet = client.open_by_key(st.secrets["ORDER_SHEET_ID"]).worksheet("Franchise Orders")
+    sheet = client.open_by_key(os.environ["ORDER_SHEET_ID"]).worksheet("Franchise Orders")
     sheet.append_row(row)
 
 
 def update_price_sheet(df):
-    sheet = client.open_by_key(st.secrets["STOCK_SHEET_ID"]).worksheet(st.secrets["PRICE_SHEET_NAME"])
+    sheet = client.open_by_key(os.environ["STOCK_SHEET_ID"]).worksheet(os.environ["PRICE_SHEET_NAME"])
     sheet.clear()
     sheet.append_row(["LN_CODE", "MRP"])
 
