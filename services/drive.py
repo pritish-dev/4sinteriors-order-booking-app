d0e14e9a2b2937f7fbeb68437e3bbc3add75ed48
Pritish Barik <pritish.sec@gmail.com>
2026-04-16 16:39:22 +0530

    drive code change

--- services/drive.py
+++ services/drive.py
@@ -1,23 +1,24 @@
 import io
 import json
-from googleapiclient.discovery import build
+import streamlit as st
 from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
 from utils.config import get_secret
 
+scope = ["https://www.googleapis.com/auth/drive"]
+
 creds_dict = json.loads(get_secret("GOOGLE_SERVICE_ACCOUNT"))
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 
-creds = Credentials.from_service_account_info(
-    creds_dict,
-    scopes=["https://www.googleapis.com/auth/drive"]
-)
-
-drive = build("drive", "v3", credentials=creds)
+drive_service = build("drive", "v3", credentials=creds)
 
 
 def get_pdfs():
     folder_id = get_secret("DRIVE_FOLDER_ID")
 
-    results = drive.files().list(
+    results = drive_service.files().list(
         q=f"'{folder_id}' in parents and mimeType='application/pdf'",
         fields="files(id, name)"
     ).execute()
@@ -26,5 +27,13 @@ def get_pdfs():
 
 
 def download_pdf(file_id):
-    request = drive.files().get_media(fileId=file_id)
-    return io.BytesIO(request.execute())
+    request = drive_service.files().get_media(fileId=file_id)
+    file = io.BytesIO()
+    downloader = MediaIoBaseDownload(file, request)
+
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+
+    file.seek(0)
+    return file
